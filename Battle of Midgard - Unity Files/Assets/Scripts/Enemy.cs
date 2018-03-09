d2a0b87db672b5462038829a08fd70d1323e2d96
Tucker Langseth <D6XB@area1.eurofins.local>
2018-03-09 17:52:14 -0500

    Adding Unity Files

---
+++ Battle of Midgard - Unity Files/Assets/Scripts/Enemy.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+
+    //initiate varialbes for enemy
+    Rigidbody2D rb = new Rigidbody2D();
+    float enemySpeed = 20.0f;
+    Vector2 initialSpeed = new Vector2(30.0f, 0);
+    float enemyHealth;
+    float enemyAttack;
+    float enemyDefense;
+    float experienceGiven;
+    
+    
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        InitialMovement(initialSpeed);
+        enemyHealth = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        EnemyDeath();
+	}
+
+    void InitialMovement(Vector2 speed)
+    {
+        rb.AddForce(speed);
+    }
+
+    void Movement(GameObject collisionObject, float speed)
+    {
+        rb.velocity = Vector2.zero;
+        GameObject nextPoint;
+        Vector3 nextPointPosition;
+        
+        //solve the last point creating an out of index error
+        if (TurnPointManager.instance.turnPoints.IndexOf(collisionObject) != TurnPointManager.instance.turnPoints.Count - 1)
+        {
+            nextPoint = TurnPointManager.instance.turnPoints[TurnPointManager.instance.turnPoints.IndexOf(collisionObject) + 1];
+            nextPointPosition = new Vector3(nextPoint.transform.position.x, nextPoint.transform.position.y);
+            Vector3 currentPointPosition = new Vector3(collisionObject.transform.position.x, collisionObject.transform.position.y);
+            Vector3 difference = nextPointPosition - currentPointPosition;
+            
+            if (TurnPointManager.instance.turnPoints.Contains(collisionObject))
+            {
+                rb.AddForce((nextPointPosition - currentPointPosition) * speed);
+            }
+        }   
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "TurnPoints")
+        {
+            Movement(collision.gameObject, enemySpeed);
+        }
+
+        if (collision.tag == "Projectile")
+        {
+            enemyHealth--;
+            collision.gameObject.SetActive(false);
+            Debug.Log(enemyHealth);
+        }
+    }
+
+
+
+    void EnemyDeath()
+    {
+        if(enemyHealth == 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Battle of Midgard - Unity Files/Assets/Scripts/ProjectileManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileManager : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent < Rigidbody2D >();
+    }
+    // Use this for initialization
+    void Start () {
+        //Give projectile force based from player stats
+		if(gameObject.activeSelf)
+        {
+            Vector3 _shootLocation = PlayerManager.instance.shootLocation;
+            float playerSpeed = 50.0f; //change based on the player speed stat
+            Debug.Log(GetComponentInParent<Transform>().position);
+            Debug.Log(_shootLocation);
+            rb.AddForce((_shootLocation - GetComponentInParent<Transform>().position) * playerSpeed);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
